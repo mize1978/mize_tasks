a67da1f3e2b1491449e039560d5b36a6041f3547
mize1978 <mizelate@gmail.com>
2026-05-04 23:51:41 +0900

    fix: allow null user_id

--- db/migrate/20260503141221_add_user_to_tasks.rb
+++ db/migrate/20260503141221_add_user_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddUserToTasks < ActiveRecord::Migration[7.0]
   def change
-    add_reference :tasks, :user, null: false, foreign_key: true
+    add_reference :tasks, :user, foreign_key: true, null: true
   end
 end
